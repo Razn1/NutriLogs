635af1b3fa3dd031fce5d18616d9669e43243f14
jeebrielibr <jeebriel.ibr@gmail.com>
2026-01-08 10:03:06 +0700

    create dummydata sql

---
+++ doc/1. Database Design/dummydata.sql
@@ -0,0 +1,23 @@
+INSERT INTO vehicles (nama_kendaraan, jenis_kendaraan, plat_nomor, kapasitas) VALUES 
+('Grand Max Blind Van', 'Mobil Box', 'B 9101 TBA', 100),
+('Carry Futura', 'Pick Up', 'D 8234 ABG', 80),
+('Traga Box', 'Truk Kecil', 'L 1122 SS', 150),
+('Hino Dutro', 'Truk Medium', 'B 4567 KAA', 300),
+('L300 Box', 'Mobil Box', 'D 9901 VBN', 120),
+('Isusu Giga', 'Truk Besar', 'B 7788 PQA', 500),
+('Motor Tossa', 'Roda 3', 'B 3344 CCZ', 30);
+INSERT INTO kitchens (nama, alamat, kapasitas, jumlah_karyawan) VALUES 
+('Dapur Pusat Merdeka', 'Jl. Industri No. 5, Jakarta Timur', 2000, 25),
+('Dapur Satelit Bandung', 'Jl. Soekarno Hatta No. 12, Bandung', 1500, 18),
+('Dapur Wilayah Surabaya', 'Kawasan Rungkut Industri III, Surabaya', 1800, 20);
+INSERT INTO schools (nama, alamat, jumlah_siswa) VALUES 
+('SDN Merdeka 01', 'Jl. Pendidikan No. 10, Jakarta Pusat', 450),
+('SMP Harapan Bangsa', 'Jl. Pemuda No. 22, Bandung', 600),
+('SMA Negeri 5 Nusantara', 'Jl. Gatot Subroto No. 5, Surabaya', 850),
+('SD Citra Kasih', 'Komp. Perumahan Indah Blok A/12, Semarang', 320),
+('SMP Bakti Utomo', 'Jl. Melati No. 45, Yogyakarta', 540),
+('SMA Bintang Kejora', 'Jl. Sudirman No. 101, Medan', 720),
+('SD Islam Al-Azhar 99', 'Jl. Kebon Jeruk No. 8, Tangerang', 400),
+('SMP Negeri 1 Bojongsoang', 'Jl. Raya Desa No. 1, Kab. Bandung', 900),
+('SMA Global Mandiri', 'Kawasan Industri Jababeka, Bekasi', 350),
+('SDN Pelita Jaya', 'Jl. Merpati No. 3, Makassar', 280);
